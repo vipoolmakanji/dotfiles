4cf001d67e813b1c476ca30f20cfe1a2d3bcfe17
Vipool Makanji <vipool.makanji@performgroup.com>
2019-03-11 17:01:14 +0000

    modify: Misc Apps list updated

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -8,9 +8,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Miscellaneous\n\n"
 
-brew_install "Android File Transfer" "android-file-transfer" "caskroom/cask" "cask"
-brew_install "eyeD3" "eye-d3"
-brew_install "Spectacle" "spectacle" "caskroom/cask" "cask"
-brew_install "Transmission" "transmission" "caskroom/cask" "cask"
+brew_install "Cyberduck" "cyberduck" "caskroom/cask" "cask"
+brew_install "Daisydisk" "daisydisk" "caskroom/cask" "cask"
+brew_install "Docker" "docker" "caskroom/cask" "cask"
+brew_install "Dropbox" "dropbox" "caskroom/cask" "cask"
+#brew_install "Filezilla" "filezilla" "caskroom/cask" "cask"
+brew_install "Postman" "postman" "caskroom/cask" "cask"
+brew_install "Purevpn" "purevpn" "caskroom/cask" "cask"
 brew_install "Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
 brew_install "VLC" "vlc" "caskroom/cask" "cask"
+brew_install "Wireshark" "wireshark" "caskroom/cask" "cask"
