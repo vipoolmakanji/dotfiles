de48a7cfdffc387f6cdd859d1884263d04639f92
Vipool Makanji <vipool.makanji@performgroup.com>
2020-05-22 22:53:07 +0100

    Change Python install

--- src/os/install/macos/python.sh
+++ src/os/install/macos/python.sh
@@ -11,9 +11,19 @@ print_in_purple "\n   Python Latest\n\n"
 # https://realpython.com/intro-to-pyenv/#installing-pyenv
 
 execute \
-  "curl https://pyenv.run | bash" \
+  "brew install pyenv pyenv-virtualenv" \
   "Install PyEnv"
 
+if [ ! -d "$HOME/.pyenv/versions/3.7.2" ]; then
+  execute \
+    "pyenv install -v 3.7.2" \
+    "Install Python 3.7.2"
+fi
+
 execute \
-  "pyenv install -v 3.7.2 && pyenv global 3.7.2" \
-  "Install Python 3.7.2"
+  "pyenv global 3.7.2" \
+  "Activate Python 3.7.2"
+
+execute \
+  "open -a Terminal -n pip.sh" \
+  "Install Pygments"
