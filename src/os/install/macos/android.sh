ca1833a60923e407414cb7e4084bc00e3e9d75d0
Vipool Makanji <vipool.makanji@dazn.com>
2021-03-16 21:44:28 +0000

    Change Oracle Java to OpenJDK

--- src/os/install/macos/android.sh
+++ src/os/install/macos/android.sh
@@ -9,7 +9,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Android Dev Tools\n\n"
 
 brew_install "Apktool" "apktool"
-brew_install "Java" "java" "--cask" "" "async-install"
+brew_install "OpenJDK Java" "adoptopenjdk" "--cask" "" "async-install"
 brew_install "Android Platform Tools" "android-platform-tools" "--cask" "" "async-install"
 brew_install "Android File Transfer" "android-file-transfer" "--cask" "" "async-install"
 brew_install "Android Studio" "android-studio" "--cask" "" "async-install"
