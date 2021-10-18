60ab53baee8e2ef203ef782a7b09570815a64156
Vipool Makanji <vipool.makanji@dazn.com>
2021-10-18 14:53:21 +0100

    pyenv fix

--- src/shell/zsh/exports/python.zsh
+++ src/shell/zsh/exports/python.zsh
@@ -4,6 +4,6 @@
 # find $HOME/.zsh/ -name \*.sh -type f -exec source '{}' \;
 export PATH="$HOME/.pyenv/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
 fi
