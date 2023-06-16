ce7d4d77972b58d4591916275e0caf145675ada6
Robin DSouza <robin.dsouza@dazn.com>
2023-06-16 06:16:35 +0100

    16062023: Updates

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -r GITHUB_REPOSITORY="vipoolmakanji/dotfiles"
+declare -r GITHUB_REPOSITORY="RobinDSouza/dotfiles"
 
 declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
 declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/master"
